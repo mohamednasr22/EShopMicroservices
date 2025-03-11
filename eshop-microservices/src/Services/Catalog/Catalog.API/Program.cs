335543a96189ab7b14ccbf0f8662c94025a8011a
nasr <31714356+mohamednasr22@users.noreply.github.com>
2025-03-11 22:25:38 +0200

    config docker successfully and still have error in  call api

--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,22 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add sevices to the container
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddCarter();
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.
+    Configuration.GetConnectionString("CatalogConnection")!);
+}).UseLightweightSessions(); 
 
 var app = builder.Build();
 
