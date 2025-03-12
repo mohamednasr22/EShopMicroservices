8e0c48e66a94c65cc24bdbe485bff32c049035f0
nasr <31714356+mohamednasr22@users.noreply.github.com>
2025-03-12 02:03:32 +0200

    get and create api

--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.Configuration;
+using Carter;
+using MediatR;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// add sevices to the container
-
+// Add services to the container
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
@@ -11,15 +13,17 @@ builder.Services.AddMediatR(config =>
     //config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     //config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
 builder.Services.AddCarter();
+
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.
-    Configuration.GetConnectionString("CatalogConnection")!);
-}).UseLightweightSessions(); 
+    opts.Connection(builder.Configuration.GetConnectionString("CatalogConnection")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
-// configure the Http request pipline.
+// Configure the HTTP request pipeline
+app.MapCarter(); // THIS LINE IS MISSING IN YOUR ORIGINAL CODE
 
 app.Run();
