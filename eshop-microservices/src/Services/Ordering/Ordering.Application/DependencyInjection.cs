36ecbc9309a3c9af201b9a955bd653c7cf1368aa
nasr <31714356+mohamednasr22@users.noreply.github.com>
2025-07-13 13:28:12 +0200

    add Validation

--- eshop-microservices/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,6 +1,6 @@
 ﻿using BuildingBlocks.Behaviors;
 //using BuildingBlocks.Messaging.MassTransit;
-using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 //using Microsoft.FeatureManagement;
 using System.Reflection;
@@ -14,8 +14,8 @@ public static class DependencyInjection
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-          //  config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-           // config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         //services.AddFeatureManagement();
