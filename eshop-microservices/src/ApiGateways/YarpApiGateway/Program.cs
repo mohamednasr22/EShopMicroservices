832a73c4a018a05fdf296a245430e47a2c45b7b6
nasr <31714356+mohamednasr22@users.noreply.github.com>
2025-09-24 02:15:05 +0200

    test after use projecr

--- eshop-microservices/src/ApiGateways/YarpApiGateway/Program.cs
+++ eshop-microservices/src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,17 +1,25 @@
-namespace YarpApiGateway
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
 {
-    public class Program
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddReverseProxy()
-                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-            var app = builder.Build();
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 
-            app.MapReverseProxy();  
+var app = builder.Build();
 
-            app.Run();
-        }
-    }
-}
+// Configure the HTTP request pipeline.
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+app.Run();
