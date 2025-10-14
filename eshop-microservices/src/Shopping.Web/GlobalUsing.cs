230675b41ccaf53de381001b27825d37d31868f4
nasr <31714356+mohamednasr22@users.noreply.github.com>
2025-10-14 02:42:31 +0200

    create index

--- eshop-microservices/src/Shopping.Web/GlobalUsing.cs
+++ eshop-microservices/src/Shopping.Web/GlobalUsing.cs
@@ -2,3 +2,6 @@
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Ordering;
 global using Refit;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Shopping.Web.Services;
